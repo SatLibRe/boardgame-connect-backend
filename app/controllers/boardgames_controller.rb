3f3314c14c8067e646ea61c8d36ad1aee6ba79e6
Miles Higbie <miles.higbie@gmail.com>
2020-03-20 14:55:18 -0400

    added room assoc to boardgame

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -2,10 +2,12 @@ class BoardgamesController < ApplicationController
 
   #https://www.boardgameatlas.com/api/search?order_by=popularity&ascending=false&client_id=s0dPEZnuUa
 
+  #include the rooms for boardgames
+
   def index 
-    rooms = Boardgame.all 
-    render json: rooms
-end 
+    boardgame = Boardgame.all 
+    render json: boardgame.to_json(include: [:rooms])
+  end 
 
 def create 
     boardgame = Boardgame.find_or_create_by(api_id: boardgame_params[:id])
