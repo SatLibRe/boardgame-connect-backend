cb3d2a1265311cbcdcf6a37522f159bc1a4df50a
arichards4814 <arichards4814@gmail.com>
2020-03-23 14:47:50 -0400

    added users_rooms route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_132805) do
+ActiveRecord::Schema.define(version: 2020_03_23_175200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_03_22_132805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "boardgame_id"
+    t.integer "maxplayers"
   end
 
   create_table "user_board_games", force: :cascade do |t|
