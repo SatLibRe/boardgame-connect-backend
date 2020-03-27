e38163fd8f391c8d5e2027f681418840580cac30
arichards4814 <arichards4814@gmail.com>
2020-03-27 11:47:50 -0400

    further authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+def get_auth_header
+    request.headers["Authorization"]
+end
+
+def session_user
+    User.find_by(id: get_auth_header)
+end
+
+
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,14 @@
 class MessagesController < ApplicationController
 
 def create 
-         message = Message.new(messages_params)
+         message = Message.new({
+            user_id: session_user.id,
+            message_content: messages_params[:message_content],
+            room_id: messages_params[:room_id]
+         })
+         puts "new message"
+         puts "new message"
+         puts message.user_id
          if message.save
             chat_room = Room.find(message.room_id)
             RoomsChannel.broadcast_to(chat_room, message)
